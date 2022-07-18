7c61b6c15925838dd67a3797c63b21d8eb5ba842
Marcel Caraballo <7672175+maqndon@users.noreply.github.com>
2022-07-18 12:28:23 +0200

    Seeders, Migrations, Factories

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class CompanyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->company(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'website' => 'https://'.$this->faker->domainName(),
+            'logo' => 'images/default.png',
+        ];
+    }
+}
