fe5cb9dfc06c2d7363cc7d38907c321cd21d2268
Marcel Caraballo <7672175+maqndon@users.noreply.github.com>
2022-07-18 16:05:39 +0200

    Employee Views

--- resources/views/employees/edit.blade.php
+++ resources/views/employees/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Edit company') }}
+            {{ __('Edit Employee') }}
         </h2>
     </x-slot>
 
@@ -10,42 +10,41 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
 
-                    {{ 'Edit Company' }}
+                    {{ 'Edit Employee' }}
                 </div>
                 
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     {{-- class mt-4 --}}
-                    <form method="POST" action="{{ route('companies.update', $company) }}" enctype="multipart/form-data">
+                    <form method="POST" action="{{ route('employees.update', $employee) }}" enctype="multipart/form-data">
                         @csrf
                         @method('PUT')
                         <div>
+                            {{-- first name --}}
+                            <x-label for="name" :value="__('First Name')" />
+                            <x-input id="name" class="block mt-1 w-full" type="text" name="fist_name" :value="$employee->first_name" required autofocus />
+                            
+                        </div>
+                        <div>
                             {{-- name --}}
-                            <x-label for="name" :value="__('Name')" />
-                            <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="$company->name" required autofocus />
+                            <x-label for="name" :value="__('Last Name')" />
+                            <x-input id="name" class="block mt-1 w-full" type="text" name="last_name" :value="$employee->last_name" required autofocus />
                             
                         </div>
                             <!-- Email Address -->
                         <div class="mt-4">
                             <x-label for="email" :value="__('Email')" />
 
-                            <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="$company->email" />
+                            <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="$employee->email" />
                         </div>
                         <div class="mt-4">
-                            <x-label for="website" :value="__('Website')" />
+                            <x-label for="website" :value="__('Phone')" />
 
-                            <x-input id="website" class="block mt-1 w-full" type="text" name="website" :value="$company->website" />
+                            <x-input id="website" class="block mt-1 w-full" type="text" name="phone" :value="$employee->phone" />
                         </div>
 
-                        @if (!$company->logo)
-                            <div class="mt-4">
-                                <x-label for="logo" :value="__('Logo')" />
-
-                                <x-input id="logo" class="block mt-1 w-full" type="file" name="logo" :value="old('$company->logo')" />
-                            </div>
-                        @endif
                         <div class="flex items-center justify-end mt-4">
                             <x-button class="ml-4">
-                                {{ __('Edit Company') }}
+                                {{ __('Edit Employee') }}
                             </x-button>
                         </div>
 
---
+++ resources/views/employees/show.blade.php
@@ -0,0 +1,45 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Employees') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <p>Employee Details</p>
+                </div>
+                {{-- <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">Companies List</div> --}}
+                <div class="p-6 text-gray-700">
+                    
+                    <div class="p-2 border">
+                        <p class="inline-block">First Name: </p> {{ $employee->first_name }}
+                    </div>
+
+                    <div class="p-2 border">
+                        <p class="inline-block">Last Name: </p> {{ $employee->last_name }}
+                    </div>
+                    
+                    <div class="p-2 border-l border-r">
+                        <p class="inline-block">Email: </p> {{ $employee->email }}
+                    </div>
+                    
+                    <div class="p-2 border">
+                        <p class="inline-block">Phone: </p> {{ $employee->phone }}
+                    </div>
+                    
+                    <form action="">
+                        <x-button class="ml-4">
+                            <a href={{ route('employees.edit', $employee->id) }}>{{ __('Edit') }}</a>
+                        </x-button>
+                    </form>
+
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</x-app-layout>
