d59f0ac67c6324e7fb1d50ddc9c638bf8ec90529
Marcel Caraballo <7672175+maqndon@users.noreply.github.com>
2022-07-18 16:42:06 +0200

    Employee Controller and Service

---
+++ app/Services/CompanyService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Company;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+
+class CompanyService
+{
+
+    public function createCompany(Request $request): Company
+    {
+
+        $imageDir = 'images';
+
+        $company = Company::create([
+            'name' => $request->name, 
+            'email' => $request->email, 
+            'website' => $request->website,
+            'logo' => $request->hasFile('logo')
+                ? $request->file('logo')
+                    ->storeAs(
+                        $imageDir,
+                        'logo_' . Str::of($request->name)->slug . '.' . $request->logo->extension()
+                        )
+                : $imageDir.'/default.png'
+        ]);
+        return $company;
+    }
+}
