63d6aafef283a5f36e2b270a537f110a653b9502
Marcel Caraballo <7672175+maqndon@users.noreply.github.com>
2022-07-18 11:07:04 +0200

    Company and Employee Models and Migrations

---
+++ app/Models/Employee.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'company_id',
+        'email',
+        'phone',
+    ];
+
+    /**
+    * Get the company that owns the Employee
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    */
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+}
