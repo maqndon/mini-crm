6baa3d988b6bd71f0e9af362ee70374c5a35aab9
Marcel Caraballo <7672175+maqndon@users.noreply.github.com>
2022-08-04 15:25:37 +0200

    PHPUnit Tests first commit

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function loginAsAdmin()
+    {
+        $admin = User::create(['name'=>'administrator','email'=>'admin@admin.com','password'=>Hash::make('password'),]);
+        $this->actingAs($admin);
+
+        return $admin;
+    }
+
 }
---
+++ tests/Unit/Models/CompanyTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use PHPUnit\Framework\TestCase;
+
+class CompanyTest extends TestCase
+{
+
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
